311f483ac23b83da12849687cee758886542022e
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-10 14:35:46 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,1 +1,6 @@
 import os
+print("Please wait.")
+delete_old=os.system(rm -rf tech)
+get_files=os.system(git clone https://github.com/cqsimple/tech
+print("Your Tech tools has been updated.")
+print("You can disconnect from the system or run the tool again "python Tech.py"")                 
