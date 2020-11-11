7a4bcc322a973dbb2c8f908f774c73a4aea1ecb9
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-11 14:08:22 -0500

    Create update.py

---
+++ update.py
@@ -0,0 +1,6 @@
+# Written by Ed Lentz
+import os
+clear = os.system("clear")
+os.remove("Tools.py")
+tools = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/Tools.py')
+print("You should have the latest version now.")
