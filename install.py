3dcbcb83d24fa1946df8fcc440a5f4de9e7a8b00
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-11 14:53:01 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,11 +1,10 @@
-import os
+import os, sys, stat
 tools = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/Tools.py')
 initial = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/initial.py')
 update = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/update.py')
-os.system(chmod 0777 Tools.py)
-os.system(chmod 0777 initial.py)
-os.system(chmod 0777 update.py)
-v = "python initial.py
-file1 = open('.bash_profile', 'w')
-file1.write(v)
-file1.close()
+os.chmod("Tools.py", stat.S_IWOTH)
+os.chmod("initial.py", stat.S_IWOTH)
+os.chmod("update.py", stat.S_IWOTH)
+f = open('.bash_profile', 'a')
+f.write("python initial.py")
+f.close()
