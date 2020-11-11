9af0646438a4c3803a57ef51147b514770b83f41
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-11 14:32:09 -0500

    Create install.py

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+tools = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/Tools.py')
+initial = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/initial.py')
+update = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/update.py')
+os.system(chmod 0777 Tools.py)
+os.system(chmod 0777 initial.py)
+os.system(chmod 0777 update.py)
+v = "python initial.py
+file1 = open('.bash_profile', 'w')
+file1.write(v)
+file1.close()
