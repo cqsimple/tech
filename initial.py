485935c8d9c0111c03e9c9e5581f28de710c1893
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-11 13:53:29 -0500

    Create initial.py

---
+++ initial.py
@@ -0,0 +1,20 @@
+# Written by Ed Lentzimport os
+clear = os.system("clear")
+print ("")
+print("CQ Simple Nimbus Command Line system.")
+print ("Use these commands with caution.")
+print("Enter the number of the command you want to run and press enter.")
+
+ans=True
+while ans:
+    print("""
+    1.Command Line Tools
+    2.Update Tools program
+        """)
+    ans=raw_input("What would you like to do? ")
+    if ans=="1":
+      check_date = os.system("python Tools.py")
+    elif ans=="2":
+      restart_ntpd = os.system("python update.py")
+    else:
+       print("\n Not a Valid Choice Try again")
