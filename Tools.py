a8326347b5787c1681d001475bd455465017189a
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-11 08:59:29 -0500

    Update and rename Tech.py to Tools.py

--- Tools.py
+++ Tools.py
@@ -18,9 +18,6 @@ while ans:
     8.Reset file permissions
     9.PM2 List
     10.Restart Clearly Devices Module
-    11.Scan Network (Local systems only)
-    12.Restart Fail2Ban (Intrusion Detection)
-    13.Restart Dashboard (FOP2)
     99.Exit/Quit
     999.Check for an update to this program
     """)
@@ -45,12 +42,6 @@ while ans:
       pm2_list = os.system("sudo fwconsole pm2 --list")
     elif ans=="10":
       restart_clearly = os.system("sudo fwconsole pm2 --restart='clearlydevices'")
-    elif ans=="11":
-      arp_scan = os.system("sudo arp-scan --interface=eth0 --localhost")
-    elif ans=="12":
-      arp_scan = os.system("sudo service fail2ban restart")
-    elif ans=="13":
-      arp_scan = os.system("sudo service fop2 restart")
     elif ans=="99":
       Quit = os.system("exit")
       clear=os.system("clear")
@@ -58,8 +49,5 @@ while ans:
       print("Your session is over.")
       print("Please disconnect from the system.")
       ans = None
-    elif ans=="999":
-      print(" this program is up to date")
-      ans = None
-    else:
+     else:
        print("\n Not a Valid Choice Try again")
