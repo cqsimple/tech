8e3e50ec9fb46814509dad67e84663ff6a146e2e
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-12 14:49:12 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,8 +1,21 @@
 # Written by Edward Lentz
-import os
-clear = os.system("clear")
-os.remove("Tools.py")
-tools = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/Tools.py')
-os.remove("initial.py")
-tools = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/initial.py')
-print("You should have the latest version now.")
+import os, urllib2
+
+filedata = urllib2.urlopen('https://raw.githubusercontent.com/cqsimple/tech/main/Tools.py')
+datatowrite = filedata.read()
+with open('Tools.py', 'wb') as f:
+    f.write(datatowrite)
+    
+filedata1 = urllib2.urlopen('https://raw.githubusercontent.com/cqsimple/tech/main/initial.py')
+datatowrite = filedata1.read()
+with open('initial.py', 'wb') as f:
+    f.write(datatowrite)
+
+filedata2 = urllib2.urlopen('https://raw.githubusercontent.com/cqsimple/tech/main/update.py')
+datatowrite = filedata2.read()
+with open('update.py', 'wb') as f:
+    f.write(datatowrite)
+
+files = ['initial.py', 'update.py', 'Tools.py']
+for file in files:
+    os.chmod(file, 0o0777)
