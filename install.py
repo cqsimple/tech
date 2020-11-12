9d5bac0f04e908617ed0c382e4971e26a5e74315
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-12 14:35:07 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,13 +1,24 @@
-import os, sys, stat
-tools = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/Tools.py')
-initial = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/initial.py')
-update = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/update.py')
+import os, sys, stat, urllib2
+
+filedata = urllib2.urlopen('https://raw.githubusercontent.com/cqsimple/tech/main/Tools.py')
+datatowrite = filedata.read()
+with open('Tools.py', 'wb') as f:
+    f.write(datatowrite)
+    
+filedata1 = urllib2.urlopen('https://raw.githubusercontent.com/cqsimple/tech/main/initial.py')
+datatowrite = filedata1.read()
+with open('initial.py', 'wb') as f:
+    f.write(datatowrite)
+
+filedata2 = urllib2.urlopen('https://raw.githubusercontent.com/cqsimple/tech/main/update.py')
+datatowrite = filedata2.read()
+with open('update.py', 'wb') as f:
+    f.write(datatowrite)
+
 files = ['initial.py', 'update.py', 'Tools.py']
 for file in files:
     os.chmod(file, 0o0777)
+
 f = open('.bash_profile', 'a')
 f.write("python initial.py")
 f.close()
-f1 = open('/etx/sudoers', 'a')
-f1.write("tech ALL=NOPASSWD: /usr/sbin/fwconsole chown,/usr/bin/yum update,/usr/sbin/fwconsole ma upgradeall,/etc/freepbx.conf,/var/lib/asterisk/bin/fwconsole,/bin/systemctl,/sbin/service ntpd restart,/sbin/arp-scan")
-f1.close()
