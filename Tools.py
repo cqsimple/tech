b63dc533be13570544f55fd3a89313b8e045a3e7
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-12 15:27:07 -0500

    Update Tools.py

--- Tools.py
+++ Tools.py
@@ -1,11 +1,11 @@
 import os
-# Written by Edward Lentz
+# Written by Edward Lentz -- Nov. 2020
 clear = os.system("clear")
 print ("")
 print("CQ Simple Nimbus Command Line Tools.")
 print ("Use these commands with caution.")
 print("Enter the number of the command you want to run and press enter.")
-
+print("When you are done you can close the SSH session.")
 ans=True
 while ans:
     print("""
