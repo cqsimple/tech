32a34486868ca1e4b4beaedd80a060fd5de00554
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-12 09:33:40 -0500

    Update update.py

--- update.py
+++ update.py
@@ -3,4 +3,6 @@ import os
 clear = os.system("clear")
 os.remove("Tools.py")
 tools = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/Tools.py')
+os.remove("initial.py")
+tools = os.system('wget https://raw.githubusercontent.com/cqsimple/tech/main/initial.py')
 print("You should have the latest version now.")
