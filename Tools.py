f572b78b3bba0fbd013b9c3c9d8757bf069f0008
cqsimple <41925078+cqsimple@users.noreply.github.com>
2020-11-12 11:30:50 -0500

    Update Tools.py

--- Tools.py
+++ Tools.py
@@ -20,29 +20,28 @@ while ans:
     9.PM2 List
     10.Restart Clearly Devices Module
     99.Exit/Quit
-    999.Check for an update to this program
     """)
     ans=raw_input("What would you like to do? ")
     if ans=="1":
       check_date = os.system("date")
     elif ans=="2":
-      restart_ntpd = os.system("sudo service ntpd restart")
+      restart_ntpd = os.system("service ntpd restart")
     elif ans=="3":
-      fwconsole_restart = os.system("sudo fwconsole restart")
+      fwconsole_restart = os.system("fwconsole restart")
     elif ans=="4":
-      Module_update = os.system("sudo fwconsole ma upgradeall")
+      Module_update = os.system("fwconsole ma upgradeall")
     elif ans=="5":
-      validate = os.system("sudo fwconsole validate")
+      validate = os.system("fwconsole validate")
     elif ans=="6":
-      clean = os.system("sudo fwconsole validate --clean")
+      clean = os.system("fwconsole validate --clean")
     elif ans=="7":
-      refresh = os.system("sudo fwconsole ma refreshsignatures")
+      refresh = os.system("fwconsole ma refreshsignatures")
     elif ans=="8":
-      file_permissions = os.system("sudo fwconsole chown")
+      file_permissions = os.system("fwconsole chown")
     elif ans=="9":
-      pm2_list = os.system("sudo fwconsole pm2 --list")
+      pm2_list = os.system("fwconsole pm2 --list")
     elif ans=="10":
-      restart_clearly = os.system("sudo fwconsole pm2 --restart='clearlydevices'")
+      restart_clearly = os.system("fwconsole pm2 --restart='clearlydevices'")
     elif ans=="99":
       Quit = os.system("exit")
       clear=os.system("clear")
