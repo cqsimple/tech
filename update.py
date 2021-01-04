3cbd05d840409a476595703f679e811f78e27316
cqsimple <41925078+cqsimple@users.noreply.github.com>
2021-01-04 10:18:57 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,5 @@
 # Written by Edward Lentz
-import os, urllib2,time
+import os, urllib2,time,shutil
 
 filedata = urllib2.urlopen('https://raw.githubusercontent.com/cqsimple/tech/main/Tools.py')
 datatowrite = filedata.read()
@@ -19,11 +19,16 @@ with open('update.py', 'wb') as f:
 filedata3 = urllib2.urlopen('https://raw.githubusercontent.com/cqsimple/tech/main/irontec.repo')
 datatowrite = filedata3.read()
 with open('irontec.repo', 'wb') as f:
-    f.write(datatowrite)    
+    f.write(datatowrite) 
+    
 
 files = ['initial.py', 'update.py', 'Tools.py', 'irontec.repo']
 for file in files:
     os.chmod(file, 0o0777)
+
+original = r'/irontec.repo'
+target = r'/etc/yum.repos.d/irontec.repo'
+shutil.move(original,target)    
     
 print("You should have the latest version now.")
 time.sleep(10)
